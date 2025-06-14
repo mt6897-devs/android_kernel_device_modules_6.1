b0691fa041f1811e81393040b7a6e1a954becbcd
rio004 <riyon336@gmail.com>
2025-06-14 14:56:26 +0100

    drivers: gpu: drm: Import MI_HWCONF_MANAGER from `bsp-rodin-v-oss`
    Change-Id: I776ddf49115723e8b69786a5122265f55a4f71aa
    Signed-off-by: rio004 <riyon336@gmail.com>

--- drivers/gpu/drm/mediatek/mediatek_v2/mi_disp/mi_hwconf_manager.h
+++ drivers/gpu/drm/mediatek/mediatek_v2/mi_disp/mi_hwconf_manager.h
@@ -7,6 +7,19 @@
 #ifndef _MI_HWCONF_MANAGER_H_
 #define _MI_HWCONF_MANAGER_H_
 
+#ifdef CONFIG_MI_HWCONF_MANAGER
+extern int register_hw_component_info(char *component_name);
+extern int add_hw_component_info(char *component_name, char *key, char *value);
+extern int unregister_hw_component_info(char *component_name);
+extern int register_hw_monitor_info(char *component_name);
+extern int add_hw_monitor_info(char *component_name, char *mon_key, char *mon_value);
+extern int update_hw_monitor_info(char *component_name, char *mon_key, char *mon_value);
+extern int hw_monitor_notifier_register(struct notifier_block *nb);
+extern int hw_monitor_notifier_unregister(struct notifier_block *nb);
+extern int unregister_hw_monitor_info(char *component_name);
+int hwconf_init(void);
+void hwconf_exit(void);
+#else
 static inline int register_hw_component_info(char *component_name) {return 0;}
 static inline int add_hw_component_info(char *component_name, char *key, char *value) {return 0;}
 static inline int unregister_hw_component_info(char *component_name) {return 0;}
@@ -18,5 +31,6 @@ static inline int hw_monitor_notifier_unregister(struct notifier_block *nb) {ret
 static inline int unregister_hw_monitor_info(char *component_name) {return 0;}
 int hwconf_init(void) { return 0; }
 void hwconf_exit(void) {}
+#endif
 
 #endif /* _MI_HWCONF_MANAGER_H_ */

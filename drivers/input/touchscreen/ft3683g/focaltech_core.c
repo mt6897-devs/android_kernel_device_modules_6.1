871c44eb79894dbfcceb055608f994cdc6719601
bengris32 <bengris32@protonmail.ch>
2025-08-29 13:46:41 +0100

    drivers: input: ft3683g: Trigger suspend on MI_DISP_DPMS_EVENT
    Change-Id: Id72d2b01d1d3226516d946e2aa0d8893357a78f1

--- drivers/input/touchscreen/ft3683g/focaltech_core.c
+++ drivers/input/touchscreen/ft3683g/focaltech_core.c
@@ -2378,7 +2378,8 @@ int fts_drm_state_change_callback(struct notifier_block *self,
 		blank = *(int *)(evdata->data);
 		FTS_INFO("notifier tp event:%lu, code:%d.", event, blank);
 		flush_workqueue(core_data->ts_workqueue);
-		if (event == MI_DISP_DPMS_EARLY_EVENT &&
+		if ((event == MI_DISP_DPMS_EARLY_EVENT ||
+		     event == MI_DISP_DPMS_EVENT) &&
 		    (blank == MI_DISP_DPMS_POWERDOWN ||
 		     blank == MI_DISP_DPMS_LP1 || blank == MI_DISP_DPMS_LP2)) {
 			FTS_INFO("touchpanel suspend by %s",
